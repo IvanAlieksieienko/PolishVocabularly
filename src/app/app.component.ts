0264e0b8ac1f3f946898cdaa85846b69793903c8
Ivan Alieksieienko <alexeenko20@gmail.com>
2022-12-17 14:05:36 +0100

    Created application, added popular verbs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import verbsData from '../assets/verbs.json';
+
+interface Verb {
+    verb: string,
+    iAndYouForm: string,
+    translate: string
+}
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Polish';
+    displayedColumns: string[] = ['verb', 'iAndYouForm', 'translate'];
+    dataSource = verbsData;
 }
